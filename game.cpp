7f2009636d5e0afe687c67c9d05a959a153d5595
imVituZeron <vitorpaulasantos@gmail.com>
2023-10-09 21:11:37 -0300

    I'm starting the course of C++

---
+++ game.cpp
@@ -0,0 +1,8 @@
+#include<iostream> // this library use to std::cout <<
+using namespace std; // this use std function to default on the all code.
+
+int main() {
+    cout << "***********************" << endl;
+    cout << "* Welcome to the Game *" << endl;
+    cout << "***********************" << endl;   
+}
