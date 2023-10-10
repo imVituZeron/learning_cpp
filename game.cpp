13965501a92bffb261f6bd66252aa1873c3752bb
imVituZeron <vitorpaulasantos@gmail.com>
2023-10-09 21:23:23 -0300

    Show secret number in a COUT

--- game.cpp
+++ game.cpp
@@ -4,5 +4,8 @@ using namespace std; // this use std function to default on the all code.
 int main() {
     cout << "***********************" << endl;
     cout << "* Welcome to the Game *" << endl;
-    cout << "***********************" << endl;   
+    cout << "***********************" << endl;
+
+    int secret_number = 42;
+    cout << "The number secret is " << secret_number << ". Don't tell it nobody!" << endl;  
 }
