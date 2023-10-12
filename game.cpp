7f414795199da2495dfd05c3288933f5299db586
imVituZeron <vitorpaulasantos@gmail.com>
2023-10-11 21:25:14 -0300

    It was created an While for more attempts and also a count of attempts

--- game.cpp
+++ game.cpp
@@ -7,20 +7,30 @@ int main() {
     cout << "***********************" << endl;
 
     const int SECRET_NUMBER = 42;
-    int kick;
-    
-    cout << "Choose one number: ";
-    cin >> kick; // input values of keyboard
-    cout << "The value of kick is "<< kick << endl;
+    bool not_right = true;
+    int attempts = 0;
 
-    bool win = kick == SECRET_NUMBER;
-    bool bigger_than_secretnumber = kick > SECRET_NUMBER;
+    while(not_right){
+        int kick;
+        attempts++;
 
-    if (win){
-        cout << "Congratulations!, You got the secret number right!" << endl;
-    } else if (bigger_than_secretnumber) {
-        cout << "Your number is bigger than secret number!" << endl;
-    } else {
-        cout << "Your number is smaller than secret number!" << endl;
+        cout << "Attempt: "<< attempts << endl; // number attempt
+        cout << "Choose one number: ";
+        cin >> kick; // input values of keyboard
+        cout << "The value of kick is "<< kick << endl;
+
+        bool win = kick == SECRET_NUMBER;
+        bool bigger_than_secretnumber = kick > SECRET_NUMBER;
+
+        if (win){
+            cout << "Congratulations!, You got the secret number right!" << endl;
+            not_right = false;
+        } else if (bigger_than_secretnumber) {
+            cout << "Your number is bigger than secret number!" << endl;
+        } else {
+            cout << "Your number is smaller than secret number!" << endl;
+        }
     }
+    cout << "You just right secret number " << attempts << " attempts" << endl;
+    cout << "End Game!" << endl;
 }
