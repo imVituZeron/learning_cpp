bb144ebf40b058a0d7cc78b74c61914a929567fb
imVituZeron <vitorpaulasantos@gmail.com>
2023-10-15 15:34:50 -0300

    it was created difficulty level determining a attempts number per level

--- game.cpp
+++ game.cpp
@@ -6,15 +6,29 @@ int main() {
     cout << "* Welcome to the Game *" << endl;
     cout << "***********************" << endl;
 
+    cout << "Choose your difficulty level: " << endl;
+    cout << "Easy (E), Middle (M), Hard (H): ";
+    char level;
+    cin >> level;
+
+    // variable and IF condition that to fill attempt_number with number;
+    int attempt_number; 
+    if (level == 'E') {
+        attempt_number = 20;
+    } else if (level == 'M') {
+        attempt_number = 10;
+    } else {
+        attempt_number = 5;
+    }
+
     const int SECRET_NUMBER = 42;
     bool not_right = true;
     int attempts = 0;
 
     double points = 1000.0; //point for person
 
-    while(not_right){
+    for (attempts = 1;attempts <= attempt_number;attempts++) {
         int kick;
-        attempts++;
 
         cout << "Attempt: "<< attempts << endl; // number attempt
         cout << "Choose one number: ";
@@ -29,17 +43,24 @@ int main() {
         points = points - points_threw_out; // I take your points.
 
         if (win){
-            cout << "Congratulations!, You got the secret number right!" << endl;
+            cout << "==> Congratulations!, You got the secret number right!" << endl;
             not_right = false;
+            break;
         } else if (bigger_than_secretnumber) {
-            cout << "Your number is bigger than secret number!" << endl;
+            cout << "==> Your number is bigger than secret number!" << endl;
         } else {
-            cout << "Your number is smaller than secret number!" << endl;
+            cout << "==> Your number is smaller than secret number!" << endl;
         }
     }
-    cout << "You just right secret number " << attempts << " attempts" << endl;
-    cout.precision(2);
-    cout << fixed;
-    cout << "Your score are: " << points << endl;
+
+    // final message!
     cout << "End Game!" << endl;
+    if (not_right) {
+        cout << "==> You lost, try again!" << endl;
+    } else {
+        cout << "==> You just right secret number " << attempts << " attempts" << endl;
+        cout.precision(2);
+        cout << fixed;
+        cout << "==> Your score are: " << points << endl;
+    }
 }
